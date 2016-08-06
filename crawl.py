dc6bb99569fd08f307585779653a83a3c7a68a01
lulu410 <qx39@cornell.edu>
2016-08-06 18:10:51 -0400

    add imput and deal with some exceptions

--- crawl.py
+++ crawl.py
@@ -1,25 +1,35 @@
 import urllib
 import re
+import sys
 import socket
 import Queue
 
-header = "file:///Users/madison/Desktop/simple-html/"
-initial_page = header + "index.html"
+initial = raw_input("Please enter a initial web page\n") 
+k = initial.rfind("/")
+header = initial[:(k+1)]
+print "header ", header, "\n"
+initial_page = initial
 seen = {}
 
 def merge_lists(x, y):
-	copy = dict(x)
-	copy.update(y)
-	return copy
+	if x == None:
+		copy = dict(x)
+		copy.update(y)
+		return copy
+	else:
+		return dict(x)
 
 def extract_url(webpage, pairs):
-	htmlfile = urllib.urlopen(webpage)
-	htmltext = htmlfile.read()
-	urls = re.findall(r'href=[\'"]?([^\'" >]+)', htmltext)
-	regex = '<a[^>]*>(.*?)</a>'
-	names_pattern = re.compile(regex)
-	names = re.findall(names_pattern, htmltext)
-	return merge_lists(dict(zip(urls, names)).items(), pairs.items())
+	try:
+		htmlfile = urllib.urlopen(webpage)
+		htmltext = htmlfile.read()
+		urls = re.findall(r'(?i)href=[\'"]?([^\'" >]+)', htmltext)
+		regex = '<[\aA][^>]*>(.*?)</[\aA]>'
+		names_pattern = re.compile(regex)
+		names = re.findall(names_pattern, htmltext)
+		return merge_lists(dict(zip(urls, names)).items(), pairs.items())
+	except Exception:
+		pass
 
 links =  extract_url(initial_page, {})
 while (len(links) > 0):
@@ -29,4 +39,4 @@ while (len(links) > 0):
 		links = merge_lists(links, new_links)
 	seen[key] = value
 	print seen
-	
+						
